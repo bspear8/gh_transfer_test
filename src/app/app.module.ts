0dcb7daf2da02682fb509777164769d95dd902a2
Blaine Spear <blaine.spear@vk.ai>
2023-11-15 14:55:52 +0900

    - Added testing component 3 for mirroring test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent2Component } from './test-component-2/test-component-2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent2Component
   ],
   imports: [
     BrowserModule,
